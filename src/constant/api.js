fe77717747f2b893e6ba057a7ca2243c1207bc4b
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-01 16:26:18 -0400

    feat: endpoints

---
+++ src/constant/api.js
@@ -0,0 +1,7 @@
+const API = 'https://localhost:8080';
+
+export const endPoints = {
+    countries: `${API}/countries`,
+    getOneCountry: `${API}/country`,
+    getByYear: `${API}/internet-users`,
+};
