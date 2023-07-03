d54595503eb98e1ab498416f84f52a32978ce350
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-02 21:27:20 -0400

    feat: hero text

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { UsersPerYearChart } from './components/UsersPerYearChart';
 import { TopCountries } from './components/TopCountries';
 import { WorldMap } from './components/WorldMap';
+import { Hero } from './components/Hero';
 
 function App() {
     return (
-        <>
-            <h1 className="text-purple-500">Hola fem hack</h1>
-
+        <div className="main h-full">
+            <Hero />
             <UsersPerYearChart />
 
             <TopCountries />
             <WorldMap />
-        </>
+        </div>
     );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,18 @@ import { Layout } from './Layout';
 export const Hero = () => {
     return (
         <Layout>
-            <div className="hero flex justify-between items-center">
-                <h1>Texto asdasdasdasdsadasd</h1>
-                <img src={heroimg} className="w-3/6" />
+            <div className="hero flex justify-between items-center gap-2">
+                <div className="flex flex-col gap-6">
+                    <h1 className="text-5xl font-bold">
+                        Every day the use of the <span className="font-extrabold text-indigo-600">Internet</span> in people's
+                        daily lives surpasses a new milestone in history.{' '}
+                    </h1>
+                    <p>
+                        The Internet has brought hyper-connectivity to the world allowing anyone in the world to contact anyone
+                        else in the world.
+                    </p>
+                </div>
+                <img src={heroimg} className="w-7/12" />
             </div>
         </Layout>
     );
