fa4fded3c870c592dce5e48b9cb6a942843b82dc
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-02 21:57:27 -0400

    style: header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,21 @@ import { Layout } from './Layout';
 export const Hero = () => {
     return (
         <Layout>
-            <div className="hero flex justify-between items-center gap-2">
-                <div className="flex flex-col gap-6">
+            <div className="hero flex flex-col justify-between items-center gap-2 md:flex-row py-20">
+                <div className="flex flex-col gap-6 md:items-start md:w-5/12">
                     <h1 className="text-5xl font-bold">
                         Every day the use of the <span className="font-extrabold text-indigo-600">Internet</span> in people's
                         daily lives surpasses a new milestone in history.{' '}
                     </h1>
-                    <p>
+                    <p className="text-xl">
                         The Internet has brought hyper-connectivity to the world allowing anyone in the world to contact anyone
-                        else in the world.
+                        else in the world!
                     </p>
+                    <button className="bg-indigo-600 text-white px-6 py-2 font-bold rounded-lg hover:bg-purple-600">
+                        Check the data!
+                    </button>
                 </div>
-                <img src={heroimg} className="w-7/12" />
+                <img src={heroimg} className="md:w-7/12 w-full" />
             </div>
         </Layout>
     );
